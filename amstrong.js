8fbcf1f19495e0ef960da4ba084fdabd4dcea4bd
johnmuthua <caveomuthama@gmail.com>
2023-09-19 07:50:51 +0300

    Added amstrong integer

--- amstrong.js
+++ amstrong.js
@@ -1,11 +1,21 @@
 
 
 function amstrong(num) {
-  while(num > 0) {
-    console.log(num % 10);
-    num = parseInt(num /10);
+  let init = num;
+  let sum = 0;
+  let power = num.toString().length;
+  let sPow;
+  num.toString().split('').forEach(c => {
+    sPow = Math.pow(parseInt(c), power);
+    sum += sPow
+  })
+  if(sum != init) {
+    console.log(false);
+    return false;
   }
-  console.log("");
+  console.log(true);
+  return true;
 }
-
+amstrong(407);
 amstrong(2123);
+amstrong(8204);
