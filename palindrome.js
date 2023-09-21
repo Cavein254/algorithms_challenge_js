a9a7787157076d879a90be994355615764fff401
johnmuthua <caveomuthama@gmail.com>
2023-09-21 17:01:50 +0300

    Add another palindrome function

--- palindrome.js
+++ palindrome.js
@@ -10,12 +10,18 @@ function palindromeOne(str) {
   return true;
 }
 
-function palindrome(str) {
+function palindromeTwo(str) {
   let nStr = str.split('').reverse().join('');
   if(nStr === str) {
     return true;
   }
   return false;
+}
+
+function palindrome(str) {
+  return str.split('').every((char, index)=> {
+    return char === str[str.length - index - 1]
+  })
 }
 
 console.log(palindrome("abba"));
