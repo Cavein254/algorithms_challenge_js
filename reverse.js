46c016f12f4ed3c4a6a20f30253e13edd7577864
johnmuthua <caveomuthama@gmail.com>
2023-09-21 14:44:53 +0300

    Add reverse array

--- reverse.js
+++ reverse.js
@@ -6,3 +6,9 @@ function Reverse(arr) {
 let arr = "This is a simple in the end";
 
 console.log("This is a sample", Reverse(arr));
+
+function ReverseTwo(arr) {
+  return arr.split().reduce((str, char) => str = str + char,'' )
+}
+
+console.log("Reverse Two", ReverseTwo(arr));
